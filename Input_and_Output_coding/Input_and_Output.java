ee05676033cd5c195a0c04500deb2538f5f10c71
dipanwita1405 <dipanwita3018@gmail.com>
2025-09-30 00:08:13 +0530

    Your descriptive commit message here

--- Input_and_Output_coding/Input_and_Output.java
+++ Input_and_Output_coding/Input_and_Output.java
@@ -4,10 +4,15 @@ public class Input_and_Output {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); // Reads an integer
+        // int n = sc.nextInt(); // Reads an integer
         String s = sc.nextLine(); // Reads a line
-        System.out.println("Number: " + n);
+      
+        // System.out.println("Number: " + n);
         System.out.println("Text: " + s);
+        System.out.println(s.getClass().getName()); 
+System.out.println(s.getClass().getSimpleName()); 
+
+
     }
 }
 
