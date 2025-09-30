a5160738f64f0ff62ad970ccb7f33d09e0b44ef2
dipanwita1405 <dipanwita3018@gmail.com>
2025-09-30 15:55:12 +0530

    Your descriptive commit message here

--- Input_and_Output_coding/Input_and_Output.js
+++ Input_and_Output_coding/Input_and_Output.js
@@ -2,14 +2,12 @@
 let name = prompt("Enter your name:");
 console.log("Hello", name);
 
-
 let age = parseInt(prompt("Enter your age:"), 10);
-
 
 console.log(`My name is ${name} and I am ${age} years old`);
 
 console.log("Name: " + name + ", Age: " + age);
 
-// 5️ Type check
+//  Type check
 console.log(typeof name); // string
 console.log(typeof(age));  // number
